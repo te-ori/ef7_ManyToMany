f49aa2170ea3c3384ad2612292cd21dc80d48df9
Orhan Aygün <orh.aygun@gmail.com>
2023-12-14 19:54:30 +0300

    many to many without foreign keys

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using M2m;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var ctx = new EfContext();
+
+        var users = ctx.Users.Include(u => u.Roles).ToList();
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"{user.Id} {user.Name}");
+            Console.WriteLine("Roles:");
+            foreach (var role in user.Roles)
+            {
+                Console.WriteLine($"{role.Id} {role.Name}");
+            }
+        }
+    }
+}
